563e7c1cc257b6a5fc9a6e828a71b4c8608ebae1
caojianyu <caojianyu@zyctd.com>
2020-10-10 17:41:15 +0800

    路由插件 支持默认路由

--- src/router.class.js
+++ src/router.class.js
@@ -6,40 +6,29 @@ export default class Router {
         const defaults = {
             mode: 'hash', //模式：hash|history
             routes: [], //路由配置
+            default: '', //默认路由
             pathChange: function (path) {},
             before: function (route, prev) {}, //路由即将进入
             ready: function (route) {}, //路由加载就绪
             fail: function (route) {}, //路由加载失败
             after: function (route, next) {}, //路由即将离开
-            notFound: function (path) {
-                console.error(`${path} 当前路由没找到`)
+            notFound: (path) => {
+                //如果页面参数没有路由信息，则跳转默认路由页面
+                if (!path && this.options.default) {
+                    setTimeout(() => {
+                        this.replace({
+                            name: this.options.default
+                        });
+                    }, 10);
+                } else {
+                    console.error(`${path} 当前路由没找到`)
+                }
             }, //路由未找到
         }
         this.options = Object.assign(defaults, options);
         this.changeFunc = null;
         this.updateFunc = null;
         this.route = null; //当前路由
-    }
-    /**
-     * 路由参数更新执行的方法
-     * @param {*} func 
-     */
-    update(func) {
-        this.updateFunc = func;
-    }
-    /**
-     * 路由改变执行的方法
-     * @param {*} func 
-     */
-    change(func) {
-        this.changeFunc = func;
-    }
-
-    onUpdate(param) {
-        this.updateFunc && typeof this.updateFunc === 'function' && this.updateFunc(param);
-    }
-    onChange(route, current) {
-        this.changeFunc && typeof this.changeFunc === 'function' && this.changeFunc(route, current);
     }
     init() {
         const options = this.options;
@@ -67,8 +56,28 @@ export default class Router {
         } else {
             console.error(`不支持的路由模式:${options.mode}`)
         }
+    }
 
+    /**
+     * 路由参数更新执行的方法
+     * @param {*} func 
+     */
+    update(func) {
+        this.updateFunc = func;
+    }
+    /**
+     * 路由改变执行的方法
+     * @param {*} func 
+     */
+    change(func) {
+        this.changeFunc = func;
+    }
 
+    onUpdate(param) {
+        this.updateFunc && typeof this.updateFunc === 'function' && this.updateFunc(param);
+    }
+    onChange(route, current) {
+        this.changeFunc && typeof this.changeFunc === 'function' && this.changeFunc(route, current);
     }
 
 
